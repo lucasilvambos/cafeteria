c1539a3864a83f4704d849630ad146a5432f17e9
lucasambos <lucasambos@gmail.com>
2025-08-07 20:25:30 -0300

    Alterei os nomes

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
-const header = document.getElementById('header');
 
-window.addEventListener('scroll', () => {
-  if (window.scrollY > 100) {
-    header.classList.add('show-header');
-  } else {
-    header.classList.remove('show-header');
-  }
+
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('.search-bar');
+  const input = form.querySelector('input[name="q"]');
+
+  form.addEventListener('submit', function (e) {
+    e.preventDefault(); // Impede o envio do formulário
+
+    const termo = input.value.toLowerCase().trim(); // Normaliza entrada
+    const produtos = document.querySelectorAll('.produtos-card');
+
+    let encontrou = false;
+
+    produtos.forEach(produto => {
+      const titulo = produto.querySelector('h3').textContent.toLowerCase();
+
+      if (titulo.includes(termo)) {
+        produto.style.display = 'block';
+        encontrou = true;
+      } else {
+        produto.style.display = 'none';
+      }
+    });
+
+    if (!encontrou) {
+      alert("Nenhum produto encontrado.");
+    }
+  });
 });
